a3ecfc3530e6da2f43a1ac3c3593597eb2324d92
Muhammed Afshan Anas <114726854+afshan5225@users.noreply.github.com>
2025-08-09 11:05:19 +0530

    Update postgresql.js

--- server/src/postgresql.js
+++ server/src/postgresql.js
@@ -1,9 +1,18 @@
-// For more information about this file see https://dove.feathersjs.com/guides/cli/databases.html
 import knex from 'knex'
 
 export const postgresql = app => {
   const config = app.get('postgresql')
-  const db = knex(config)
+
+  // Modify connection to include SSL
+  const knexConfig = {
+    client: config.client,
+    connection: {
+      connectionString: config.connection,
+      ssl: { rejectUnauthorized: false }
+    }
+  }
+
+  const db = knex(knexConfig)
 
   app.set('postgresqlClient', db)
 }
